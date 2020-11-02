a03b13371e06a3f8079d730ab4c30d4ded6ad4a2
Linkraft <jordan.smiths.games@gmail.com>
2020-11-01 21:53:45 -0500

    Messing around

---
+++ Assets/Scripts/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patient : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public IEnumerator WaitForEnd(AudioClip audioClip)
+    {
+        this.audioSource.clip = audioClip;
+        //this.animator.SetBool("Talking", true);
+        this.audioSource.Play();
+        yield return new WaitForSeconds(audioClip.length);
+        //this.animator.SetBool("Talking", false);
+    }
+}
