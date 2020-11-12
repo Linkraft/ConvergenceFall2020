b1301cd76f03674473c24b063571fe7d7cfdc54a
ines said <inessaid@mail.usf.edu>
2020-11-12 18:38:14 -0500

    menu

---
+++ Assets/buttonhandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonhandler : MonoBehaviour
+{
+    public void VRButtonClick()
+    {
+        SceneManager.LoadScene("MobileVR1");
+    }
+    public void ARButtonClick()
+    {
+        SceneManager.LoadScene("MobileAR");
+       // SceneManager.LoadScene("MobileAR");
+
+    }
+    public void InfoButtonClick()
+    {
+       
+    }
+
+
+}
